c82410a02012917f46470f14fbd28567e6a99966
Dwiki Fajar Kurniawan <dwikifkurniawan@gmail.com>
2025-07-08 20:58:03 +0700

    fix: correct mask tensor shape for segmentation evaluation on coco_eval

--- src/data/coco/coco_eval.py
+++ src/data/coco/coco_eval.py
@@ -115,7 +115,8 @@ class CocoEvaluator(object):
             labels = prediction["labels"].tolist()
 
             rles = [
-                mask_util.encode(np.array(mask[0, :, :, np.newaxis], dtype=np.uint8, order="F"))[0]
+                # mask_util.encode(np.array(mask[0, :, :, np.newaxis], dtype=np.uint8, order="F"))[0]
+                mask_util.encode(np.array(mask.unsqueeze(-1).cpu(), dtype=np.uint8, order="F"))[0]
                 for mask in masks
             ]
             for rle in rles:
